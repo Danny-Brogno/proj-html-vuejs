d08092200bd7ac79179b710c174e4966196d3da9
Danny <danny.brogno@gmail.com>
2021-05-08 16:41:22 +0200

    progressing

--- js/script.js
+++ js/script.js
@@ -213,12 +213,82 @@ function vueInit() {
             "price": 47,
             "image": "img/other-img/spring_printed_dress-120x156.jpg"
         }
-      ]
+      ],
+
+      boxesInFooter: [
+        {
+            "name": "1"
+        },
+        {
+            "name": "2"
+        },
+        {
+            "name": "3"
+        },
+        {
+            "name": "4"
+        },
+        {
+            "name": "5"
+        },
+        {
+            "name": "6"
+        },
+        {
+            "name": "7"
+        },
+        {
+            "name": "8"
+        },
+        {
+            "name": "9"
+        },
+        {
+            "name": "10"
+        },
+        {
+            "name": "11"
+        },
+        {
+            "name": "12"
+        },
+        {
+            "name": "13"
+        },
+        {
+            "name": "14"
+        },
+        {
+            "name": "15"
+        },
+        {
+            "name": "16"
+        },
+        {
+            "name": "17"
+        },
+        {
+            "name": "18"
+        },
+        {
+            "name": "19"
+        },
+        {
+            "name": "20"
+        },
+        {
+            "name": "21"
+        },
+        {
+            "name": "22"
+        }
+      ],
 
     }, // END OF DATA
 
     methods: {
-      getStar: function() {
+
+      getStar: function(stars) {
         return 5;
       }, // END OF getStar FUNCTION
 
